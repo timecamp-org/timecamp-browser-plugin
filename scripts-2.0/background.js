20e3271ca1325151d67989393bda697117c71b7b
kamilbator <kamildamianbator@gmail.com>
2021-07-26 09:14:52 +0200

    [TCD-1656] Fix after CR

--- scripts-2.0/background.js
+++ scripts-2.0/background.js
@@ -540,8 +540,8 @@ browser.runtime.onInstalled.addListener((details) => {
     if (details.reason === 'install') {
         showInstructionsPage();
     } else if (details.reason === 'update') {
-        let thisVersion = browser.runtime.getManifest().version;
-        let versionWhenActionIsPerformed = '2.21.1';
+        const thisVersion = browser.runtime.getManifest().version;
+        const versionWhenActionIsPerformed = '2.21.1';
         if (thisVersion === versionWhenActionIsPerformed && details.previousVersion !== versionWhenActionIsPerformed) {
             showInstructionsPage();
         }
