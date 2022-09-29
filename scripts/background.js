4134853a36f83677f3001eeb384dc14dba6ed42d
chenten16 <mfilali455@gmail.com>
2022-09-29 11:50:45 +0100

    adding google form on uninstall

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,20 @@
+const SURVEY_URL = "https://forms.gle/dfhvjtahyjf9w8bWA"
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
         if (request.id === "apiService") {
             $.ajax(request.params).success(function (data) {
-                sendResponse({resolve: true, data: data});
+                sendResponse({ resolve: true, data: data });
             }).error(function (xhr) {
                 if (xhr.status && xhr.status === 403 && !request.isRetry) {
-                     TokenManager.obtainNewToken().done(function (token) {
-                         ApiService.setToken(token);
-                         sendResponse({resolve: false, data: {retry: request}});
-                     }).fail(function () {
-                         sendResponse({resolve: false, data: xhr});
-                     });
+                    TokenManager.obtainNewToken().done(function (token) {
+                        ApiService.setToken(token);
+                        sendResponse({ resolve: false, data: { retry: request } });
+                    }).fail(function () {
+                        sendResponse({ resolve: false, data: xhr });
+                    });
                 } else {
-                    sendResponse({resolve: false, data: xhr});
+                    sendResponse({ resolve: false, data: xhr });
                 }
             });
 
@@ -25,7 +27,7 @@ chrome.runtime.onMessage.addListener(
                 type: "post",
                 contentType: 'application/json',
                 dataType: 'json',
-                data: JSON.stringify({type: "chrome-plugin-start-timer", details: request.response})
+                data: JSON.stringify({ type: "chrome-plugin-start-timer", details: request.response })
             });
 
             return true;
@@ -33,9 +35,9 @@ chrome.runtime.onMessage.addListener(
 
         if (request.id === "canTrack") {
             $.ajax(request.params).done(function (response) {
-                sendResponse({resolve: true, data: response});
+                sendResponse({ resolve: true, data: response });
             }).fail(function () {
-                sendResponse({resolve: false, data: null});
+                sendResponse({ resolve: false, data: null });
             });
 
             return true;
@@ -47,7 +49,7 @@ chrome.runtime.onMessage.addListener(
                 data: request.params.data,
                 type: 'POST'
             }).done(function (token) {
-                if(token.toUpperCase() !== 'NO_SESSION' && token.length <= 50) {
+                if (token.toUpperCase() !== 'NO_SESSION' && token.length <= 50) {
                     TokenManager.storeToken(token);
                     chrome.runtime.sendMessage({
                         popup: {
@@ -55,7 +57,7 @@ chrome.runtime.onMessage.addListener(
                             statusText: chrome.i18n.getMessage("STATUS_SUCCESS")
                         }
                     });
-                    setTimeout(function(){
+                    setTimeout(function () {
                         window.parent.close();
                     }, 3000);
                 } else {
@@ -78,3 +80,5 @@ chrome.runtime.onMessage.addListener(
             });
         }
     });
+
+chrome.runtime.setUninstallURL(SURVEY_URL)
