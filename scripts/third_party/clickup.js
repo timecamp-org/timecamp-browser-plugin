26e0e59c3dfdbb1aa590668052b68296a19887e4
Mohammed Filali <59532610+chenten16@users.noreply.github.com>
2024-01-17 13:23:15 +0100

    [BP-84] The "Start timer" button from the plugin does not appear in ClickUp 3.0 (#120)
    * add extra selector
    
    * Clickup fix
    
    ---------
    
    Co-authored-by: bpDevTc <104766829+bpDevTc@users.noreply.github.com>

--- scripts/third_party/clickup.js
+++ scripts/third_party/clickup.js
@@ -96,7 +96,14 @@ function ClickupTimer () {
                 $this.buttonInsertionInProgress = false;
             });
 
-        var clickupUserTaskHeaderSection = $('.task__toolbar').find('.cu-task-header__section')[0];
+        var clickupUserTaskHeaderSection = $(".task__toolbar").find( ".cu-task-header__section" )[0];
+        
+        if (!clickupUserTaskHeaderSection) {
+          clickupUserTaskHeaderSection = document
+              .querySelector('[data-test="task-hero-section__row"]')
+              ?.querySelector(".cu-task-hero-section__row-item");
+        }
+        
         $(button).insertAfter(clickupUserTaskHeaderSection);
         buttonObj.insertInProgress = false;
 
