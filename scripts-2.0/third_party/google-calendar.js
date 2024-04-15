ddc3a4cd2bc63bc780f488ecf3d74665942765ab
bpDevTc <104766829+bpDevTc@users.noreply.github.com>
2024-04-15 15:48:10 +0200

    CR fix

--- scripts-2.0/third_party/google-calendar.js
+++ scripts-2.0/third_party/google-calendar.js
@@ -1,39 +1,44 @@
-"use strict";
+'use strict';
 
-const GOOGLE_CALENDAR = "googlecalendar";
+const GOOGLE_CALENDAR = 'googlecalendar';
 
 //Table view
-tcbutton.render(".i5a7ie:not(.tc)", { observe: true }, (elem) => {
-  function createTimecampElements() {
-    const getDescription = () => {
-      const descriptionSelector = $('[role="heading"]', elem);
-      if (descriptionSelector) return descriptionSelector.textContent;
-      else return "";
-    };
+tcbutton.render(
+    '.i5a7ie:not(.tc)',
+    { observe: true },
+    (elem) => {
+        function createTimecampElements() {
+            const getDescription = () => {
+                const descriptionSelector = $('[role="heading"]', elem);
+                if (descriptionSelector) {
+                    return descriptionSelector.textContent;
+                }
 
-    let cardHeader = $(".wv9rPe");
+                return '';
+            };
+
+            let cardHeader = $(".wv9rPe");
     
-    if (cardHeader) {
-        const link = tcbutton.createTimerLink({
-            className: GOOGLE_CALENDAR,
-            additionalClasses: [GOOGLE_CALENDAR + '__entry'],
-            description: getDescription(),
-            isBackendIntegration: true
+            if (cardHeader) {
+                const link = tcbutton.createTimerLink({
+                    className: GOOGLE_CALENDAR,
+                    additionalClasses: [GOOGLE_CALENDAR + '__entry'],
+                    description: getDescription(),
+                    isBackendIntegration: true
+                });
+
+                const timecampContainer = createTag("div", "tc-widget-container");
+
+                timecampContainer.appendChild(link);
+                cardHeader.appendChild(timecampContainer);
+            }
+        }
+        if (!$(".tc-widget-container")) {
+            createTimecampElements();
+        }
+        $$('[role="button"][data-opens-details="true"]').forEach((item) => {
+            item.addEventListener("click", (event) => {
+                createTimecampElements();
+            });
         });
-      
-    
-      const timecampContainer = createTag("div", "tc-widget-container");
-
-      timecampContainer.appendChild(link);
-      cardHeader.appendChild(timecampContainer);
-    }
-  }
-  if (!$(".tc-widget-container")) {
-    createTimecampElements();
-  }
-  $$('[role="button"][data-opens-details="true"]').forEach((item) => {
-    item.addEventListener("click", (event) => {
-        createTimecampElements();
     });
-  });
-});
