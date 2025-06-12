e0b0b03ef2457f8fac14c1dd1eb9a4e01761a7be
kamil-rudnicki <kamil.rudnicki@gmail.com>
2025-06-12 08:24:16 +0200

    Added linear

---
+++ scripts-2.0/third_party/linear.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const LINEAR = 'linear';
+
+tcbutton.render(
+    'div[data-view-id="issue-view"]:not(.tc)',
+    { observe: true },
+    function ($container) {
+        if ($container.querySelector('.tc-button')) {
+            return;
+        }
+
+        const title = $container.querySelector('[aria-label="Issue title"]')?.textContent;
+        const projectElem = $container.parentElement.parentElement.querySelector(
+            'svg[aria-label="Project"]'
+        );
+        const project = projectElem?.nextElementSibling?.textContent?.trim();
+
+        const link = tcbutton.createTimerLink({
+            className: LINEAR,
+            description: title || 'Linear Issue',
+            projectName: project
+        });
+
+        const sidebar = $container.parentElement.parentElement.lastElementChild.firstElementChild;
+        if (sidebar && sidebar.lastElementChild) {
+            // sidebar.lastElementChild.appendChild(link);
+            sidebar.lastElementChild.insertAdjacentElement('afterbegin', link);
+        }
+
+        return true;
+    }
+);
