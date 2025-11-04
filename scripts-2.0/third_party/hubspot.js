18a470ca3416f6c3a38d00c46e843a4089840a0a
bpDevTc <104766829+bpDevTc@users.noreply.github.com>
2025-11-04 11:33:31 +0100

    [TCD-10705] Hubspot fix

--- scripts-2.0/third_party/hubspot.js
+++ scripts-2.0/third_party/hubspot.js
@@ -3,7 +3,7 @@
 const SERVICE = 'hubspot';
 
 tcbutton.render(
-    'td[data-table-external-id*="name-"] [data-test-id="truncated-object-label"]:not(.tc)',
+    'td[data-table-external-id*="name-"][data-column-index="0"] [data-test-id="truncated-object-label"]:not(.tc)',
     { observe: true, debounceInterval: 500 },
     elem => {
         const getDescription = () => {
